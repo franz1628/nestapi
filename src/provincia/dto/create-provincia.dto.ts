8d2e3b35abb90a957c248bfdf32cbdd1b83e07ec
Franz Wilder <gualambo@gmail.com>
2024-12-31 22:34:23 -0500

    quitando min 3

--- src/provincia/dto/create-provincia.dto.ts
+++ src/provincia/dto/create-provincia.dto.ts
@@ -3,7 +3,6 @@ import { IsInt, IsNotEmpty, IsString, Length, Max, Min } from "class-validator";
 export class CreateProvinciaDto {
     @IsNotEmpty()
     @IsString()
-    @Length(3, 255)
     descripcion: string;
   
     @IsNotEmpty()
